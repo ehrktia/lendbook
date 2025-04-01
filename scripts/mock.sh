40a08c54284f1dc45f1e2149d5f31081aeadce68
karthick <a.karthie+git@pm.me>
2025-04-01 17:15:36 +0100

    update mock script with filepath

--- scripts/mock.sh
+++ scripts/mock.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/bash
 clear
 set -euo pipefail
-echo -n "generating mocks..."
+echo "removing mock files from ${ROOT_DIR}/mocks"
+ls -lart ${ROOT_DIR}/mocks
+rm ${ROOT_DIR}/mocks/*.go
+echo  "generating mocks in ${ROOT_DIR}/mocks..."
 mockery --dir internal --outpkg mocks --output mocks --all \
     --exclude internal/graph --with-expecter=true
+echo "mock files successfully generated."
+ls -lart ${ROOT_DIR}/mocks
